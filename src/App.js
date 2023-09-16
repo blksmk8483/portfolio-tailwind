a078baefdc7f9300896772fb1d800a95662c0782
Brandon Knight <knightbrandon1984@gmail.com>
2023-09-15 23:13:14 -0500

    navbar is complete 🙌

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
-      <p className="text-8xl font-bold">Hello there...</p>
+      <Navbar />
     </div>
   );
 }
