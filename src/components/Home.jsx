ffb3a4b160eca54a9120bc9af0fcc8a3f69e2a86
Brandon Knight <knightbrandon1984@gmail.com>
2023-09-16 01:30:50 -0500

    inverted the background color and changed the button color to match the end color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,12 @@ export const Home = () => {
   return (
     <div
       name="home"
-      className="w-full h-screen bg-gradient-to-r from-violet-500 to-fuchsia-500 "
+      // className="w-full h-screen bg-gradient-to-r from-violet-500 to-fuchsia-500 "
+      className="w-full h-screen bg-gradient-to-r from-fuchsia-600 to-violet-600 "
     >
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-pink-600">Hi, my name is</p>
+        <p className="text-violet-800">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           Brandon Knight
         </h1>
@@ -22,7 +23,7 @@ export const Home = () => {
           React, and Tailwind CSS.
         </p>
         <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-violet-800 hover:border-violet-800">
             View Work
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3" />
