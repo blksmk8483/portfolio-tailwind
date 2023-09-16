75a17bd6ea79b2ba542d46dcf89a76ed50212b54
Brandon Knight <knightbrandon1984@gmail.com>
2023-09-15 23:56:30 -0500

    added Social Icons to Navbar

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+export const Home = () => {
+  return (
+    <div className="w-full h-screen bg-gradient-to-r from-violet-500 to-fuchsia-500 ">
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/profileTransparent.png";
 
 const Navbar = () => {
@@ -46,7 +48,42 @@ const Navbar = () => {
       </ul>
 
       {/* Social Icons */}
-      <div className="hidden"></div>
+      <div className="hidden md:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="www.linkedin.com/in/brandonknight8483"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              GitHub <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
