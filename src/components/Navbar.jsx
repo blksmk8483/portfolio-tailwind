d9d6a2a0007ebb6f4f73f13ec618a32f26a5088d
Brandon Knight <knightbrandon1984@gmail.com>
2023-09-17 03:01:55 -0500

    added resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/profileTransparent.png";
 import { Link } from "react-scroll";
+import Resume from "../assets/Resume.png";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -154,7 +155,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href={Resume}
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
